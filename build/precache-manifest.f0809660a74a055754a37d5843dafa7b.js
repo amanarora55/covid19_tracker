b1ce839b1ba19ad0768120ba479bb2fb35836bfe
amanarora55 <amanarora@paralleldots.com>
2020-04-30 11:59:42 +0530

    routes with gh pages

---
+++ build/precache-manifest.f0809660a74a055754a37d5843dafa7b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3d913361a5f5d0ee254cb8b45cc2c42",
+    "url": "/covid19_tracker/index.html"
+  },
+  {
+    "revision": "7dd3fa32e75335a85aae",
+    "url": "/covid19_tracker/static/css/2.a43b59a1.chunk.css"
+  },
+  {
+    "revision": "93edea956d6372e79e06",
+    "url": "/covid19_tracker/static/css/main.7ca7cd45.chunk.css"
+  },
+  {
+    "revision": "7dd3fa32e75335a85aae",
+    "url": "/covid19_tracker/static/js/2.5ed2347c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/covid19_tracker/static/js/2.5ed2347c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "93edea956d6372e79e06",
+    "url": "/covid19_tracker/static/js/main.3221b8b0.chunk.js"
+  },
+  {
+    "revision": "b20eccce2a634b03bcaf",
+    "url": "/covid19_tracker/static/js/runtime-main.4aab7bef.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/covid19_tracker/static/media/logo.5d5d9eef.svg"
+  }
+]);
